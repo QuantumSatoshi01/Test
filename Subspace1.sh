487dbe66c877eac39ea285798b87c1d40c1c3621
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-08-19 22:10:32 +0300

    Create Subspace1.sh

---
+++ Subspace1.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+function logo() {
+bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
+}
+
+function printGreen {
+  echo -e "\e[1m\e[32m${1}\e[0m"
+}
+
+
+VERSION=$(echo 'BEGIN {
+    while (!/flags/) if (getline < "/proc/cpuinfo" != 1) exit 1
+    if (/lm/&&/cmov/&&/cx8/&&/fpu/&&/fxsr/&&/mmx/&&/syscall/&&/sse2/) level = 1
+    if (level == 1 && /cx16/&&/lahf/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/) level = 2
+    if (level == 2 && /avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/) level = 3
+    if (level == 3 && /avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/) level = 4
+    if (level > 0) { print level; exit level + 1 }
+    exit 1
+}' | awk -f -)
+
+if [[ $VERSION -ne 2 && $VERSION -ne 3 ]]
+  then
+    printGreen "Скрипт не підтримує встановлення на вашому сервері, ваш процесор застарілий."
+    exit
+fi
+
+function install() {
+printGreen "Розпочалось встановлення Subpsace Gemini 3f"
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+	echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
+fi
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
+    . $HOME/.bash_profile
+fi
+
+sudo apt update && sudo apt install ocl-icd-opencl-dev libopencl-clang-dev libgomp1 -y
+cd $HOME
+wget -O pulsar https://github.com/subspace/pulsar/releases/download/v0.6.0-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.0-alpha
+sudo chmod +x pulsar
+sudo mv pulsar /usr/local/bin/
+sudo rm -rf $HOME/.config/pulsar
+/usr/local/bin/pulsar init
+#systemctl stop subspaced subspaced-farmer &>/dev/null
+#rm -rf ~/.local/share/subspace*
+
+#source ~/.bash_profile
+sleep 1
+
+echo "[Unit]
+Description=Subspace Node
+After=network.target
+
+[Service]
+User=$USER
+Type=simple
+ExecStart=/usr/local/bin/pulsar farm --verbose
+Restart=on-failure
+LimitNOFILE=1024000
+
+[Install]
+WantedBy=multi-user.target" > $HOME/subspaced.service
+
+sudo mv $HOME/subspaced.service /etc/systemd/system/
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable subspaced
+sudo systemctl restart subspaced
+
+
+if [[ `service subspaced status | grep active` =~ "running" ]]; then
+  printGreen "Subspace Gemini 3f успішно встановлено"
+  printGreen "Корисні команди:"
+  echo "Перевірити статус ноди - systemctl status subspaced"
+  echo "Журнал логів - journalctl -u subspaced -f -o cat"
+else
+  printGreen "Нода Subspace не встановлено, спробуйте встановити ще раз."
+fi
+
+logo
+install
+touch $HOME/.sdd_Subspace_do_not_remove
+logo
