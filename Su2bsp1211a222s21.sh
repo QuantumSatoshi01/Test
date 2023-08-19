dd2aa13651132a57653b5a4caebcdbaccbb563d8
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-08-20 00:11:50 +0300

    Update and rename Su2bsp1211a222s1.sh to Su2bsp1211a222s21.sh

--- Su2bsp1211a222s21.sh
+++ Su2bsp1211a222s21.sh
@@ -10,29 +10,24 @@ function printGreen {
 
 logo
 
-function update() {
-VERSION=$(echo 'BEGIN {
-    while (!/flags/) if (getline < "/proc/cpuinfo" != 1) exit 1
-    if (/lm/&&/cmov/&&/cx8/&&/fpu/&&/fxsr/&&/mmx/&&/syscall/&&/sse2/) level = 1
-    if (level == 1 && /cx16/&&/lahf/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/) level = 2
-    if (level == 2 && /avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/) level = 3
-    if (level == 3 && /avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/) level = 4
-    if (level > 0) { print level; exit level + 1 }
-    exit 1
-}' | awk -f -)
+printGreen "Під час встановлення ваша нода видалиться та перевстановиться на актуальну мережу Gemini 3f. Ви згідні? (Y/N): "
+read choice
 
-if [[ $VERSION -ne 2 && $VERSION -ne 3 ]]
-  then
-    printGreen "Скрипт не підтримує встановлення на вашому сервері, ваш процесор застарілий."
-    exit
+if [[ "$choice" == "Y" || "$choice" == "y" ]]; then
+  update
+elif [[ "$choice" == "N" || "$choice" == "n" ]]; then
+  printGreen "Ви відмовилися від перевстановлення ноди."
+else
+  printGreen "Невірний вибір. Будь ласка, введіть Y або N."
 fi
 
+function update() {
 printGreen "Видалення застарілої версії мережі Subspace Gemini 3e" && sleep 2
 sudo systemctl stop subspaced 
-  sudo systemctl disable subspaced
-  sudo rm -rf ~/.local/share/subspace*
-  sudo rm -rf /etc/systemd/system/subspace*
-  sudo rm -rf /usr/local/bin/subspace*
+sudo systemctl disable subspaced
+sudo rm -rf ~/.local/share/subspace*
+sudo rm -rf /etc/systemd/system/subspace*
+sudo rm -rf /usr/local/bin/subspace*
   
 printGreen "Розпочалось встановлення Subpsace Gemini 3f"
 exists()
@@ -95,22 +90,11 @@ echo ""
 else
   printGreen "Нода Subspace не встановлено, спробуйте встановити ще раз."
 fi
-}
 
 if [ -f $HOME/.sdd_Subspace_do_not_remove ]; then
   delete
 fi
 
 touch $HOME/.sdd_Subspace_do_not_remove
+
 }
-
-printGreen "Під час встановлення ваша нода видалиться та перевстановиться на актуальну мережу Gemini 3f. Ви згідні? (Y/N): "
-read choice
-
-if [[ "$choice" == "Y" || "$choice" == "y" ]]; then
-  update
-elif [[ "$choice" == "N" || "$choice" == "n" ]]; then
-  printGreen "Ви відмовилися від перевстановлення ноди."
-else
-  printGreen "Невірний вибір. Будь ласка, введіть Y або N."
-fi
