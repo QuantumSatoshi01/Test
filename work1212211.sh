0cbcfc18c6e2df36a1cd56c0d9e0ec58a8df5af1
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-08-22 23:35:18 +0300

    Update and rename work112211.sh to work1212211.sh

--- work1212211.sh
+++ work1212211.sh
@@ -78,26 +78,13 @@ printGreen "Starting service and synchronization..." && sleep 1
 
 SNAP_NAME=$(curl -s https://snapshots1-testnet.nodejumper.io/lava-testnet/info.json | jq -r .fileName)
 curl "https://snapshots1-testnet.nodejumper.io/lava-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C "$HOME/.lava"
+COSMOS_PORT=12
+echo "export COSMOS_PORT=${COSMOS_PORT}" >> $HOME/.profile
+source $HOME/.profile
 
-livepeers=$(curl -s https://services.bccnodes.com/testnets/lava/peers.txt)
-
-# Замінити порти в списку пірів
-NEW_PEERS=$(echo "$livepeers" | sed "s/\:[0-9]\{1,5\}/:9490/g" | \
-                                 sed "s/\:[0-9]\{1,5\}/:9491/g" | \
-                                 sed "s/\:[0-9]\{1,5\}/:30658/g" | \
-                                 sed "s/\:[0-9]\{1,5\}/:30657/g" | \
-                                 sed "s/\:[0-9]\{1,5\}/:30656/g" | \
-                                 sed "s/\:[0-9]\{1,5\}/:6460/g" | \
-                                 sed "s/\:[0-9]\{1,5\}/:30660/g" | \
-                                 sed "s/\:[0-9]\{1,5\}/:1717/g")
-
-
-sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$NEW_PEERS\"/" ~/.lava/config/config.toml
-
-
-
-
-
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${COSMOS_PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${COSMOS_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${COSMOS_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${COSMOS_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${COSMOS_PORT}660\"%" $HOME/.lava/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${COSMOS_PORT}317\"%; s%^address = \":8080\"%address = \":${COSMOS_PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${COSMOS_PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${COSMOS_PORT}091\"%; s%^address = \"0.0.0.0:8545\"%address = \"0.0.0.0:${COSMOS_PORT}545\"%; s%^ws-address = \"0.0.0.0:8546\"%ws-address = \"0.0.0.0:${COSMOS_PORT}546\"%" $HOME/.lava/config/app.toml
+sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:${COSMOS_PORT}657\"%" $HOME/.lava/config/client.toml
 sudo systemctl daemon-reload
 sudo systemctl enable lavad
 sudo systemctl start lavad
