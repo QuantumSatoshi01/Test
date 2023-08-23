066ac05efbdfdaff56f75a2b729d9a156c24830d
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-08-23 15:54:56 +0300

    Update and rename LavaTEST22.sh to LavaTES2T22.sh

--- LavaTES2T22.sh
+++ LavaTES2T22.sh
@@ -71,28 +71,6 @@ printGreen "Завантажуємо снепшот для прискоренн�
 SNAP_NAME=$(curl -s https://snapshots1-testnet.nodejumper.io/lava-testnet/info.json | jq -r .fileName)
 curl "https://snapshots1-testnet.nodejumper.io/lava-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C "$HOME/.lava"
 
-seeds="3a445bfdbe2d0c8ee82461633aa3af31bc2b4dc0@testnet2-seed-node.lavanet.xyz:26656,e593c7a9ca61f5616119d6beb5bd8ef5dd28d62d@testnet2-seed-node2.lavanet.xyz:26656"
-
-sudo systemctl daemon-reload
-sudo systemctl enable lavad
-sudo systemctl start lavad && sleep 5
-
-sed -i 's/^proxy_app = "tcp:\/\/127.0.0.1:26658"$/proxy_app = "tcp:\/\/127.0.0.1:30658"/' $HOME/.lava/config/config.toml
-sed -i 's/^laddr = "tcp:\/\/127.0.0.1:26657"$/laddr = "tcp:\/\/127.0.0.1:30657"/' $HOME/.lava/config/config.toml
-sed -i 's/^pprof_laddr = "localhost:6060"$/pprof_laddr = "localhost:6460"/' $HOME/.lava/config/config.toml
-sed -i 's/^laddr = "tcp:\/\/0.0.0.0:26656"$/laddr = "tcp:\/\/0.0.0.0:30656"/' $HOME/.lava/config/config.toml
-external_address=$(wget -qO- eth0.me)
-sed -i "s/^external_address =.*$/external_address = \"$external_address:30656\"/" $HOME/.lava/config/config.toml
-sed -i 's/^prometheus_listen_addr = ":26660"$/prometheus_listen_addr = ":30660"/' $HOME/.lava/config/config.toml
-
-sed -i 's/^address = "tcp:\/\/0.0.0.0:1317"$/address = "tcp:\/\/0.0.0.0:1717"/' $HOME/.lava/config/app.toml
-sed -i 's/^address = ":8080"$/address = ":8070"/' $HOME/.lava/config/app.toml
-sed -i 's/^address = "0.0.0.0:9090"$/address = "0.0.0.0:9490"/' $HOME/.lava/config/app.toml
-sed -i 's/^address = "0.0.0.0:9091"$/address = "0.0.0.0:9491"/' $HOME/.lava/config/app.toml
-
-sed -i 's/^node = "tcp:\/\/localhost:26657"$/node = "tcp:\/\/localhost:30657"/' $HOME/.lava/config/client.toml
-
-
 sed -i \
     -e 's/timeout_commit = ".*"/timeout_commit = "30s"/g' \
     -e 's/timeout_propose = ".*"/timeout_propose = "1s"/g' \
@@ -104,8 +82,9 @@ sed -i \
     -e 's/skip_timeout_commit = ".*"/skip_timeout_commit = false/g' \
     $HOME/.lava/config/client.toml
 
-    systemctl restart lavad && sleep 5 
-    journalctl -u lavad -f -o cat
+sudo systemctl daemon-reload
+sudo systemctl enable lavad
+sudo systemctl start lavad && sleep 5
     
 printDelimiter
 printGreen "Check logs:            sudo journalctl -u lavad -f -o cat"
