0edfad804961d7bf559bcae9364c04160f0499e9
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-08-24 15:27:44 +0300

    Update and rename Lava3333233.sh to Lava4.sh

--- Lava4.sh
+++ Lava4.sh
@@ -20,49 +20,42 @@ source $HOME/.profile
 
 source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/Nibiru/Dependencies.sh)
 
-sudo apt update # In case of permissions error, try running with sudo
-sudo apt install -y unzip logrotate git jq sed wget curl coreutils systemd
-
 git clone https://github.com/lavanet/lava-config.git
 cd lava-config/testnet-2
-# Read the configuration from the file
-# Note: you can take a look at the config file and verify configurations
 source setup_config/setup_config.sh
+
 echo "Lava config file path: $lava_config_folder"
 mkdir -p $lavad_home_folder
 mkdir -p $lava_config_folder
 cp default_lavad_config_files/* $lava_config_folder
-# Copy the genesis.json file to the Lava config folder
-# Set and create the lavad binary path
+
+cp genesis_json/genesis.json $lava_config_folder/genesis.json
+
 lavad_binary_path="$HOME/go/bin/"
 mkdir -p $lavad_binary_path
-# Download the genesis binary to the lava path
 wget https://lava-binary-upgrades.s3.amazonaws.com/testnet-2/genesis/lavad
 chmod +x lavad
-# Lavad should now be accessible from PATH, to verify, try running
 cp lavad /usr/local/bin
-# In case it is not accessible, make sure $lavad_binary_path is part of PATH (you can refer to the "Go installation" section)
+
+echo "[Unit]
+Description=Lava Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which lavad) start --home=$lavad_home_folder --p2p.seeds $seed_node
+Restart=always
+RestartSec=180
+LimitNOFILE=infinity
+LimitNPROC=infinity
+[Install]
+WantedBy=multi-user.target" >lavad.service
+sudo mv lavad.service /lib/systemd/system/lavad.service
 
   sleep 1
   lavad config keyring-backend test
   lavad config chain-id $CHAIN_ID
   lavad init "$NODE_MONIKER" --chain-id $CHAIN_ID
 
-sleep 5
-
-sudo tee /etc/systemd/system/lavad.service > /dev/null << EOF
-[Unit]
-Description=Lava Node
-After=network-online.target
-[Service]
-User=$USER
-ExecStart=$(which lavad) start
-Restart=on-failure
-RestartSec=10
-LimitNOFILE=10000
-[Install]
-WantedBy=multi-user.target
-EOF
 
 printGreen "Завантажуємо снепшот для прискорення синхронізації ноди..." && sleep 1
 
