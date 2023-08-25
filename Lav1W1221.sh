1432df23ffefa9c716853280e9dc52ee15eec7d8
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-08-25 14:24:43 +0300

    Update and rename Lav1W121.sh to Lav1W1221.sh

--- Lav1W1221.sh
+++ Lav1W1221.sh
@@ -30,7 +30,7 @@ echo "Lava config file path: $lava_config_folder"
 mkdir -p $lavad_home_folder
 mkdir -p $lava_config_folder
 cp default_lavad_config_files/* $lava_config_folder
-
+cp genesis_json/genesis.json $lava_config_folder/genesis.json
 lavad_binary_path="$HOME/go/bin/"
 mkdir -p $lavad_binary_path
 chmod +x lavad
