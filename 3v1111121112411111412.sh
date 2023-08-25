50e946719f0553cf1453757bb6888f66134696ad
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-08-25 15:23:08 +0300

    Update 3v1111121112411111412.sh

--- 3v1111121112411111412.sh
+++ 3v1111121112411111412.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 function install() {
+clear
 function printDelimiter {
   echo "==========================================="
 }
@@ -11,84 +12,70 @@ function printGreen {
 
 source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
 
-source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/Nibiru/Ports.sh) && sleep 3
-export -f selectPortSet && selectPortSet
-
-read -r -p "Enter node moniker: " NODE_MONIKER
-
 CHAIN_ID=lava-testnet-2
 echo "export CHAIN_ID=${CHAIN_ID}" >> $HOME/.profile
 source $HOME/.profile
 
-go_package_url="https://go.dev/dl/go1.20.5.linux-amd64.tar.gz"
-go_package_file_name=${go_package_url##*\/}
-# Download GO
-wget -q $go_package_url
-# Unpack the GO installation file
-sudo tar -C /usr/local -xzf $go_package_file_name
-# Environment adjustments
-echo "export PATH=\$PATH:/usr/local/go/bin" >>~/.profile
-echo "export PATH=\$PATH:\$(go env GOPATH)/bin" >>~/.profile
-source ~/.profile
-
-echo "" && printGreen "Building binaries..." && sleep 1
-
-sudo apt update # In case of permissions error, try running with sudo
-sudo apt install -y unzip logrotate git jq sed wget curl coreutils systemd
+source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/Nibiru/Dependencies.sh)
 
 git clone https://github.com/lavanet/lava-config.git
 cd lava-config/testnet-2
-# Read the configuration from the file
-# Note: you can take a look at the config file and verify configurations
 source setup_config/setup_config.sh
+
 echo "Lava config file path: $lava_config_folder"
 mkdir -p $lavad_home_folder
 mkdir -p $lava_config_folder
 cp default_lavad_config_files/* $lava_config_folder
-# Copy the genesis.json file to the Lava config folder
-# Set and create the lavad binary path
+
+
 lavad_binary_path="$HOME/go/bin/"
 mkdir -p $lavad_binary_path
-# Download the genesis binary to the lava path
 wget https://lava-binary-upgrades.s3.amazonaws.com/testnet-2/genesis/lavad
 chmod +x lavad
-# Lavad should now be accessible from PATH, to verify, try running
 cp lavad /usr/local/bin
-# In case it is not accessible, make sure $lavad_binary_path is part of PATH (you can refer to the "Go installation" section)
 
   sleep 1
   lavad config keyring-backend test
   lavad config chain-id $CHAIN_ID
-  lavad init "$NODE_MONIKER" --chain-id $CHAIN_ID
 
-sleep 10
+sudo sed -i 's/pprof_laddr = "0\.0\.0\.0:6060"/pprof_laddr = "0\.0\.0\.0:6160"/' $HOME/.lava/config/config.toml
+sudo sed -i 's/laddr = "tcp:\/\/0\.0\.0\.0:26657"/laddr = "tcp:\/\/0\.0\.0\.0:16657"/' $HOME/.lava/config/config.toml
+sudo sed -i 's/node = "tcp:\/\/localhost:26657"/node = "tcp:\/\/localhost:16657"/' $HOME/.lava/config/client.toml
+sudo sed -i 's/address = "tcp:\/\/0\.0\.0\.0:1317"/address = "tcp:\/\/0\.0\.0\.0:1327"/' "$HOME/.lava/config/app.toml"
+sudo sed -i -e "s|address = \"0.0.0.0:9090\"|address = \"0.0.0.0:19090\"|; s|address = \"0.0.0.0:9091\"|address = \"0.0.0.0:19091\"|" $HOME/.lava/config/app.toml
+sudo sed -i 's|laddr = "tcp://0.0.0.0:26656"|laddr = "tcp://0.0.0.0:16656"|' $HOME/.lava/config/config.toml
 
-sudo tee /etc/systemd/system/lavad.service > /dev/null << EOF
-[Unit]
+echo "[Unit]
 Description=Lava Node
 After=network-online.target
 [Service]
 User=$USER
-ExecStart=$(which lavad) start
-Restart=on-failure
-RestartSec=10
-LimitNOFILE=10000
+ExecStart=$(which lavad) start --home=$lavad_home_folder --p2p.seeds $seed_node
+Restart=always
+RestartSec=180
+LimitNOFILE=infinity
+LimitNPROC=infinity
 [Install]
-WantedBy=multi-user.target
-EOF
+WantedBy=multi-user.target" >lavad.service
+sudo mv lavad.service /lib/systemd/system/lavad.service
 
-printGreen "Starting service and synchronization..." && sleep 1
+printGreen "Завантажуємо снепшот для прискорення синхронізації ноди..." && sleep 1
 
 SNAP_NAME=$(curl -s https://snapshots1-testnet.nodejumper.io/lava-testnet/info.json | jq -r .fileName)
 curl "https://snapshots1-testnet.nodejumper.io/lava-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C "$HOME/.lava"
 
+rm -rf $HOME/lava-config
+
 sudo systemctl daemon-reload
 sudo systemctl enable lavad
-sudo systemctl start lavad
+sudo systemctl start lavad && sleep 5
+
 
 printDelimiter
-printGreen "Check logs:            sudo journalctl -u lavad -f -o cat"
-printGreen "Check synchronization: lavad status 2>&1 | jq .SyncInfo.catching_up"
+printGreen "Переглянути журнал логів:         sudo journalctl -u lavad -f -o cat"
+printGreen "Переглянути статус синхронізації: lavad status 2>&1 | jq .SyncInfo.catching_up"
+printGreen "Порти які використовує ваша нода: 16656,16657,6160,1327,19090,19091"
+printGreen "В журналі логів спочатку ви можете побачити помилку Connection is closed. Але за 5-10 секунд нода розпочне синхронізацію"
 printDelimiter
 
 }
