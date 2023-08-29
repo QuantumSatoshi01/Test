8bfb73a7e389b36baf8e268c8c48de10739859f4
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-08-29 16:58:34 +0300

    Update and rename 22222222.sh to 222222222.sh

--- nibiru/222222222.sh
+++ nibiru/222222222.sh
@@ -12,14 +12,6 @@ function printDelimiter {
   echo "==========================================="
 }
 
-function printDelimiter {
-  echo "==========================================="
-}
-
-function printGreen {
-  echo -e "\e[1m\e[32m${1}\e[0m"
-}
-
 function install() {
 clear
 logo
