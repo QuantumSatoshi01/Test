38c7f8c2a07ea7efda54d21e5bdb4101278a27d4
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-08-29 16:00:35 +0300

    Update and rename test11111.sh to test111111.sh

--- nibiru/test111111.sh
+++ nibiru/test111111.sh
@@ -19,18 +19,19 @@ printGreen "Введіть ім'я для вашої ноди:"
 read -r NODE_MONIKER
 
 CHAIN_ID="nibiru-itn-1"
-echo "export CHAIN_ID=${CHAIN_ID}" >> $HOME/.profile
-source $HOME/.profile
+CHAIN_DENOM="unibi"
+BINARY_NAME="nibid"
+BINARY_VERSION_TAG="v0.19.2"
 
 source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/Nibiru/Dependencies.sh)
 
-cd $HOME
-rm -rf ~/nibiru
+cd || return
+rm -rf nibiru
 git clone https://github.com/NibiruChain/nibiru
-cd nibiru
-git fetch
+cd nibiru || return
 git checkout v0.19.2
 make install
+nibid version # v0.19.2
 
 nibid config keyring-backend test
 nibid config chain-id $CHAIN_ID
@@ -45,22 +46,43 @@ sudo sed -i 's|node = "tcp://localhost:28657"|node = "tcp://localhost:15657"|' $
 sudo sed -i 's/address = "tcp:\/\/0\.0\.0\.0:1317"/address = "tcp:\/\/0\.0\.0\.0:1227"/' "$HOME/.nibid/config/app.toml"
 sudo sed -i -e "s|address = \"0.0.0.0:9090\"|address = \"0.0.0.0:18090\"|; s|address = \"0.0.0.0:9091\"|address = \"0.0.0.0:18091\"|" $HOME/.nibid/config/app.toml
 sudo sed -i 's|laddr = "tcp://0.0.0.0:26656"|laddr = "tcp://0.0.0.0:15656"|' $HOME/.nibid/config/config.toml
+PEERS=""
+sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.nibid/config/config.toml
 
-sudo tee /etc/systemd/system/nibid.service > /dev/null <<EOF
-[Unit]
-Description=nibid Node
-After=network.target
+sed -i 's|^pruning *=.*|pruning = "custom"|g' $HOME/.nibid/config/app.toml
+sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $HOME/.nibid/config/app.toml
+sed -i 's|^pruning-interval *=.*|pruning-interval = "10"|g' $HOME/.nibid/config/app.toml
+sed -i 's|^snapshot-interval *=.*|snapshot-interval = 0|g' $HOME/.nibid/config/app.toml
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001unibi"|g' $HOME/.nibid/config/app.toml
+sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.nibid/config/config.toml
+sudo sed -i 's|pprof_laddr = "localhost:6060"|pprof_laddr = "localhost:6260"|' $HOME/.nibid/config/config.toml
+sudo sed -i 's|laddr = "tcp://127.0.0.1:26657"|laddr = "tcp://127.0.0.1:15657"|' $HOME/.nibid/config/config.toml
+sudo sed -i 's|node = "tcp://localhost:28657"|node = "tcp://localhost:15657"|' $HOME/.nibid/config/client.toml
+sudo sed -i 's/address = "tcp:\/\/0\.0\.0\.0:1317"/address = "tcp:\/\/0\.0\.0\.0:1227"/' "$HOME/.nibid/config/app.toml"
+sudo sed -i -e "s|address = \"0.0.0.0:9090\"|address = \"0.0.0.0:18090\"|; s|address = \"0.0.0.0:9091\"|address = \"0.0.0.0:18091\"|" $HOME/.nibid/config/app.toml
+sudo sed -i 's|laddr = "tcp://0.0.0.0:26656"|laddr = "tcp://0.0.0.0:15656"|' $HOME/.nibid/config/config.toml
+wget $GENESIS -O $NODE_HOME/config/genesis.json
+$BINARY tendermint unsafe-reset-all
 
 [Service]
 User=$USER
 Type=simple
-ExecStart=$(which nibid) start
-Restart=on-failurerm
+ExecStart=/usr/local/bin/$BINARY start
+Restart=on-failure
 LimitNOFILE=65535
 
 [Install]
-WantedBy=multi-user.target
+WantedBy=multi-user.target" > $HOME/$BINARY.service
+sudo mv $HOME/$BINARY.service /etc/systemd/system
+sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
+Storage=persistent
 EOF
+echo -e '\n\e[42mRunning a service\e[0m\n' && sleep 1
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable $BINARY
+sudo systemctl restart $BINARY
 
 nibid tendermint unsafe-reset-all --home $HOME/.nibid --keep-addr-book
 
