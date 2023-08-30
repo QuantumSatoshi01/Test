a01e3e7f2eacd8f2680dbb3a76069da6b331165a
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-08-30 17:23:29 +0300

    Update test1.sh

--- Holograph/test1.sh
+++ Holograph/test1.sh
@@ -1,1 +1,40 @@
+#!/bin/bash
 
+function logo() {
+bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
+}
+
+function printGreen {
+  echo -e "\e[1m\e[32m${1}\e[0m"
+}
+
+function printDelimiter {
+  echo "==========================================="
+}
+clear
+logo
+printGreen "Перед початком встановлення вам потрібно виконати всі попередні кроки з гайда, а саме:"
+echo "1. Додати всі мережі собі в Metamask"
+echo "2. Запросити тестові токени на всі тестові мережі"
+echo "3. Створити RPC в Alchemy"
+echo ""
+read -p "Ви виконали всі необхідні пункти і готові продовжити встановлення ноди? [Т/Н]: " answer
+
+if [ "$answer" = "Т" ] || [ "$answer" = "т" ]; then
+    printGreen "Розпочалось встановлення Holograph..."
+    install
+else
+    echo "Процес встановлення скасовано."
+    exit 1
+fi
+
+function install() {
+printGreen "Встановлення необхідних програмних компонентів..."
+
+apt update && apt upgrade -y
+apt install curl -y
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+apt install nodejs -y
+
+printGreen "Встановлення Holograph..."
+npm install -g @holographxyz/cli
