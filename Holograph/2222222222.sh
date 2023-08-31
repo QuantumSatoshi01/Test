06e9da8f43e248505db15f200c2d455bfd7de7be
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-08-31 13:11:17 +0300

    Update and rename 222222222.sh to 2222222222.sh

--- Holograph/2222222222.sh
+++ Holograph/2222222222.sh
@@ -36,8 +36,10 @@ echo ""
 
 apt update && apt upgrade -y
 apt install curl -y
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-apt install nodejs -y
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 
 echo ""
 printGreen "Встановлення Holograph..."
