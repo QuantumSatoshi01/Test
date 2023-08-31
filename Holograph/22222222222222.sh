e0127c167e5074f60982ffafc57473853fb2f43d
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-08-31 13:42:57 +0300

    Update and rename 2222222222222.sh to 22222222222222.sh

--- Holograph/22222222222222.sh
+++ Holograph/22222222222222.sh
@@ -41,7 +41,7 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update && sudo apt-get install nodejs -y
-$ sudo apt install npm
+sudo apt install npm
 
 
 echo ""
@@ -55,11 +55,10 @@ printGreen "Та перейдіть до гайду, для виконання �
 echo "" && sleep 3
 holograph config
 
-if [ $? -eq 0 ]; then
-  for i in {1..3}; do
-printGreen "Запрошуємо тестові токени у кожну з тестових мереж"
-    holograph_faucet
-    exit 1
+ if [ $? -eq 0 ]; then
+    for i in {1..3}; do
+      holograph_faucet
+    done
   fi
 }
 
