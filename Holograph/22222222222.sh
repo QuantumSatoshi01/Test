5ad3a060f549784c498afe7ed66e67b689909398
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-08-31 13:27:14 +0300

    Update and rename 2222222222.sh to 22222222222.sh

--- Holograph/22222222222.sh
+++ Holograph/22222222222.sh
@@ -36,9 +36,11 @@ echo ""
 
 apt update && apt upgrade -y
 apt install curl -y
-sudo apt-get install curl
-curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update && sudo apt-get install nodejs -y
 
 
 echo ""
