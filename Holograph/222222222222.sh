b8cf254d778a00a2c897935dddd8681f858618ce
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-08-31 13:34:31 +0300

    Update and rename 22222222222.sh to 222222222222.sh

--- Holograph/222222222222.sh
+++ Holograph/222222222222.sh
@@ -41,6 +41,10 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update && sudo apt-get install nodejs -y
+rm -rf ~/.nvm
+export NVM_DIR=
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v$INSTALL_NVM_VER/install.sh | bash
+source ~/.nvm/nvm.sh
 
 
 echo ""
