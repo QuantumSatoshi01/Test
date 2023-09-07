de89c9847bbd06cceb2b214e28b426a0573a9cdf
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-09-07 15:39:45 +0300

    Update and rename backup111111.sh to backup1111111.sh

--- backup1111111.sh
+++ backup1111111.sh
@@ -26,7 +26,7 @@ function backup() {
         cp "/root/.lava/config/node_key.json" "$lava_backup_dir/"
         cp "/root/.lava/data/priv_validator_state.json" "$lava_backup_dir/"
         echo ""
-        printGreen "Файли ноди Lava перенесено в папку /root/BACKUPNODES/Lava backup" && sleep 2
+        printGreen "Файли ноди Lava скопійовано в папку /root/BACKUPNODES/Lava backup" && sleep 2
 
         gear_backup_dir="$backup_dir/Gear backup"
         mkdir -p "$gear_backup_dir"
@@ -37,7 +37,7 @@ function backup() {
             if [ -f "$gear_file_to_copy" ]; then
                 cp "$gear_file_to_copy" "$gear_backup_dir/"
                 echo ""
-                printGreen "Файли ноди Gear перенесено в папку /root/BACKUPNODES/Gear backup" && sleep 2
+                printGreen "Файли ноди Gear скопійовано в папку /root/BACKUPNODES/Gear backup" && sleep 2
             fi
         done
 
@@ -50,7 +50,7 @@ function backup() {
             if [ -f "$subspace_file_to_copy" ]; then
                 cp "$subspace_file_to_copy" "$subspace_backup_dir/"
                 echo ""
-                printGreen "Бекап файли ноди Subspace перенесено в папку /root/BACKUPNODES/Subspace backup" && sleep 2
+                printGreen "Бекап файли ноди Subspace скопійовано в папку /root/BACKUPNODES/Subspace backup" && sleep 2
             fi
         done
 
@@ -61,7 +61,26 @@ function backup() {
         cp "/root/.nibid/config/node_key.json" "$nibiru_backup_dir/"
         cp "/root/.nibid/data/priv_validator_state.json" "$nibiru_backup_dir/"
         echo ""
-        printGreen "Файли ноди Nibiru перенесено в папку /root/BACKUPNODES/Nibiru backup" && sleep 2
+        printGreen "Файли ноди Nibiru скопійовано в папку /root/BACKUPNODES/Nibiru backup" && sleep 2
+        echo ""
+        echo "Backup завершено, перейдіть до основної директорії /root/BACKUPNODES та скопіюйте цю папку в безпечне місце собі на ПК."
+        echo "Нижче вказано шлях до директорій, куди потрібно перенести ваші backup файли на новий сервер. В залежності від вашої ноди."
+        printGreen "Lava:"
+        echo "/root/.lava/data/priv_validator_state.json"
+        echo "/root/.lava/config/node_key.json"
+        echo "/root/.lava/config/priv_validator_key.json"
+        echo ""
+        printGreen "Gear:"
+        echo "/root/.local/share/gear/chains/gear_staging_testnet_v7/network/"
+        echo ""
+        printGreen "Subspace:"
+        echo "/root/.local/share/pulsar/node/chains/subspace_gemini_3f/network/"
+        echo ""
+        printGreen "Nibiru:"
+        echo "/root/.nibid/data/priv_validator_state.json"
+        echo "/root/.nibid/config/node_key.json"
+        echo "/root/.nibid/config/priv_validator_key.json"
+        echo ""
     else
         printGreen "Процес backup нод відмінено."
     fi
