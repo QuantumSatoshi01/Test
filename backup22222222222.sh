c803c708062ea2651af17fa48e5b3d4f067ea8fc
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-09-07 17:04:46 +0300

    Update and rename backup2222222222.sh to backup22222222222.sh

--- backup22222222222.sh
+++ backup22222222222.sh
@@ -11,7 +11,7 @@ function printGreen {
 clear
 logo
 
-printGreen "Бажаєте зробити backup нод: Lava,Nibiru,Subspace,Gear? (Y/N)"
+printGreen "Бажаєте зробити backup нод: Lava, Nibiru, Subspace, Gear? (Y/N)"
 read response
 
 function backup() {
@@ -22,11 +22,12 @@ function backup() {
         lava_backup_dir="$backup_dir/Lava backup"
         mkdir -p "$lava_backup_dir"
         lava_files_to_copy=( "/root/.lava/config/priv_validator_key.json" "/root/.lava/config/node_key.json" "/root/.lava/data/priv_validator_state.json" )
+        lava_copied=false
+
         for lava_file_to_copy in "${lava_files_to_copy[@]}"; do
             if [ -f "$lava_file_to_copy" ]; then
-            printGreen "Копіюємо backup файли ноди Lava в папку /root/BACKUPNODES/Lava backup" && sleep 2
-            cp "$lava_file_to_copy" "$lava_backup_dir/"
-            echo ""
+                cp "$lava_file_to_copy" "$lava_backup_dir/"
+                lava_copied=true
             fi
         done
 
@@ -35,11 +36,12 @@ function backup() {
 
         gear_source_dir="/root/.local/share/gear/chains/gear_staging_testnet_v7/network/" 
         gear_files_to_copy=( "$gear_source_dir/secret_ed"* )
+        gear_copied=false
+
         for gear_file_to_copy in "${gear_files_to_copy[@]}"; do
             if [ -f "$gear_file_to_copy" ]; then
-                printGreen "Копіюємо backup файли ноди Gear в папку /root/BACKUPNODES/Gear backup" && sleep 2
                 cp "$gear_file_to_copy" "$gear_backup_dir/"
-                echo ""
+                gear_copied=true
             fi
         done
 
@@ -48,36 +50,45 @@ function backup() {
 
         subspace_source_dir="/root/.local/share/pulsar/node/chains/subspace_gemini_3f/network/"
         subspace_files_to_copy=( "$subspace_source_dir/secret_ed"* )
+        subspace_copied=false
+
         for subspace_file_to_copy in "${subspace_files_to_copy[@]}"; do
             if [ -f "$subspace_file_to_copy" ]; then
-                printGreen "Копіюємо backup файли ноди Subspace в папку /root/BACKUPNODES/Subspace backup" && sleep 2
                 cp "$subspace_file_to_copy" "$subspace_backup_dir/"
-                echo ""
+                subspace_copied=true
             fi
         done
 
         nibiru_backup_dir="$backup_dir/Nibiru backup"
         mkdir -p "$nibiru_backup_dir"
-        printGreen "Копіюємо backup файли ноди Nibiru в папку /root/BACKUPNODES/Nibiru backup" && sleep 2
+
         cp "/root/.nibid/config/priv_validator_key.json" "$nibiru_backup_dir/"
         cp "/root/.nibid/config/node_key.json" "$nibiru_backup_dir/"
         cp "/root/.nibid/data/priv_validator_state.json" "$nibiru_backup_dir/"
+
         echo ""
         echo ""
         echo "Backup завершено, перейдіть до основної директорії /root/BACKUPNODES та скопіюйте цю папку в безпечне місце собі на ПК."
         echo ""
         echo "Нижче вказано шлях до директорій, куди потрібно перенести ваші backup файли на новий сервер. В залежності від вашої ноди."
-        printGreen "Lava:"
-        echo "/root/.lava/data/priv_validator_state.json"
-        echo "/root/.lava/config/node_key.json"
-        echo "/root/.lava/config/priv_validator_key.json"
-        echo ""
-        printGreen "Gear:"
-        echo "/root/.local/share/gear/chains/gear_staging_testnet_v7/network/"
-        echo ""
-        printGreen "Subspace:"
-        echo "/root/.local/share/pulsar/node/chains/subspace_gemini_3f/network/"
-        echo ""
+
+        if [ "$lava_copied" = true ]; then
+            printGreen "Lava:"
+            echo "/root/.lava/data/priv_validator_state.json"
+            echo "/root/.lava/config/node_key.json"
+            echo "/root/.lava/config/priv_validator_key.json"
+        fi
+
+        if [ "$gear_copied" = true ]; then
+            printGreen "Gear:"
+            echo "/root/.local/share/gear/chains/gear_staging_testnet_v7/network/"
+        fi
+
+        if [ "$subspace_copied" = true ]; then
+            printGreen "Subspace:"
+            echo "/root/.local/share/pulsar/node/chains/subspace_gemini_3f/network/"
+        fi
+
         printGreen "Nibiru:"
         echo "/root/.nibid/data/priv_validator_state.json"
         echo "/root/.nibid/config/node_key.json"
