6b5e6519c4fd6de81b38dd919fc1e96e15e02f0e
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-09-17 00:33:45 +0300

    Update and rename NibiITN22222.sh to NibiITN222222.sh

--- NibiITN222222.sh
+++ NibiITN222222.sh
@@ -24,76 +24,66 @@ CHAIN_DENOM="unibi"
 BINARY_NAME="nibid"
 BINARY_VERSION_TAG="v0.21.9"
 
+source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/Nibiru/Dependencies.sh)
+
 echo ""
 printGreen "Встановлюємо бінарні файли ноди"
 
-source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/Nibiru/Dependencies.sh)
-apt install curl iptables build-essential git wget jq make gcc nano tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev -y
-
-cd || return
+cd $HOME || return
 rm -rf nibiru
 git clone https://github.com/NibiruChain/nibiru
-cd nibiru || return
-git checkout v0.21.9
+cd $HOME/nibiru || return
+git checkout $BINARY_VERSION_TAG
 make install
+nibid version # 0.21.9
 
-nibid config keyring-backend test
+nibid config keyring-backend os
 nibid config chain-id $CHAIN_ID
 nibid init "$NODE_MONIKER" --chain-id $CHAIN_ID
 
-printGreen "Завантажуємо addrbook та genesis.json..." && sleep 1
-wget -O $HOME/.nibid/config/genesis.json "https://networks.itn2.nibiru.fi/nibiru-itn-2/genesis"
-wget -O $HOME/.nibid/config/addrbook.json "https://share101.utsa.tech/nibiru/addrbook.json"
+curl -s https://snapshots-testnet.stake-town.com/nibiru/genesis.json > $HOME/.nibid/config/genesis.json
+curl -s https://snapshots-testnet.stake-town.com/nibiru/addrbook.json > $HOME/.nibid/config/addrbook.json
 
 sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:30658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:30657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:6460\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:30656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":30660\"%" $HOME/.nibid/config/config.toml && sed -i.bak -e "s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:9490\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:9491\"%; s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:1717\"%; s%^address = \"0.0.0.0:8545\"%address = \"0.0.0.0:8945\"%; s%^ws-address = \"0.0.0.0:8546\"%ws-address = \"0.0.0.0:8946\"%; s%^address = \"127.0.0.1:8545\"%address = \"127.0.0.1:8945\"%; s%^ws-address = \"127.0.0.1:8546\"%ws-address = \"127.0.0.1:8946\"%" $HOME/.nibid/config/app.toml && sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:30657\"%" $HOME/.nibid/config/client.toml 
 
-sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.025unibi\"/;" ~/.nibid/config/app.toml
-external_address=$(wget -qO- eth0.me)
-peers="e36ada54e3d1e7c05c1c3b585b4235134aa185ef@65.108.206.118:60656"
-sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" $HOME/.nibid/config/config.toml
+printGreen "Встановлюємо Cosmovisor" && sleep 1
 
-sed -i 's/max_num_inbound_peers =.*/max_num_inbound_peers = 40/g' $HOME/.nibid/config/config.toml
-sed -i 's/max_num_outbound_peers =.*/max_num_outbound_peers = 10/g' $HOME/.nibid/config/config.toml
-sed -i -e "s/^filter_peers *=.*/filter_peers = \"true\"/" $HOME/.nibid/config/config.toml
-SNAP_RPC=https://rpc.itn-2.nibiru.fi:443
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
+mkdir -p ~/.nibid/cosmovisor/genesis/bin
+mkdir -p ~/.nibid/cosmovisor/upgrades
+cp ~/go/bin/nibid $HOME/.nibid/cosmovisor/genesis/bin
 
-LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
-BLOCK_HEIGHT=$((LATEST_HEIGHT - 1000)); \
-TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
-
-echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
-
-sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
-s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
-s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
-s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" $HOME/.nibid/config/config.toml
 printGreen "Завантажуємо снепшот для прискорення синхронізації ноди..." && sleep 1
-source $HOME/.bash_profile
 
-tee /etc/systemd/system/nibid.service > /dev/null <<EOF
+sudo tee /etc/systemd/system/nibid.service > /dev/null << EOF
 [Unit]
-Description=nibid
+Description=Nibiru Node
 After=network-online.target
-
 [Service]
 User=$USER
-ExecStart=$(which nibid) start
+ExecStart=$(which cosmovisor) run start
 Restart=on-failure
 RestartSec=3
-LimitNOFILE=65535
-
+LimitNOFILE=10000
+Environment="DAEMON_NAME=nibid"
+Environment="DAEMON_HOME=$HOME/.nibid"
+Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"
+Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
+Environment="UNSAFE_SKIP_BACKUP=true"
 [Install]
 WantedBy=multi-user.target
 EOF
 
+nibid tendermint unsafe-reset-all --home $HOME/.nibid --keep-addr-book
 
+
+URL="https://snapshots-testnet.stake-town.com/nibiru/nibiru-itn-2_latest.tar.lz4"
+curl $URL | lz4 -dc - | tar -xf - -C $HOME/.nibid
+[[ -f $HOME/.nibid/data/upgrade-info.json ]]  && cp $HOME/.nibid/data/upgrade-info.json $HOME/.nibid/cosmovisor/genesis/upgrade-info.json
 
 sudo systemctl daemon-reload
 sudo systemctl enable nibid
 sudo systemctl start nibid
-
-
 
 printDelimiter
 printGreen "Переглянути журнал логів:         sudo journalctl -u nibid -f -o cat"
