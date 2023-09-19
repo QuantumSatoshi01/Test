28b093242e853de726e51bc2f4776b8df2c817c0
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-09-19 22:01:18 +0300

    Create gear.sh

---
+++ gear.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+function logo() {
+    bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
+}
+
+function printGreen {
+    echo -e "\e[1m\e[32m${1}\e[0m"
+}
+
+function check {
+    while true; do
+        logo
+        printGreen "Виберіть, що ви хочете переглянути в ноді Gear:"
+        echo "1) Журнал логів"
+        echo "2) Статус ноди"
+        echo "3) Версію встановленої ноди"
+        echo "4) Назву вашої ноди"
+        echo "5) Вийти з меню"
+        read choice
+
+        if [[ $choice == "1" ]]; then
+            echo ""
+            printGreen "Журнал логів Gear. Натисніть CTRL+C щоб вийти."
+            echo ""
+            journalctl -n 100 -f -u gear
+            echo ""
+        elif [[ $choice == "2" ]]; then
+            echo ""
+            printGreen "Статус ноди"
+            echo ""
+            systemctl status gear
+            echo ""
+        elif [[ $choice == "3" ]]; then
+            echo ""
+            version=$(./gear --version)
+            echo "Версія вашої ноди: $version"
+            echo ""
+        elif [[ $choice == "4" ]]; then
+            echo ""
+            cat /etc/systemd/system/gear.service
+            echo ""
+        elif [[ $choice == "5" ]]; then
+            break 
+        else
+            echo "Невірний вибір."
+        fi
+    done
+}
+
+check
