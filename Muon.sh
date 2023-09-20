a70090433041e1b9df80ae5cee0117981bfbc9ce
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-09-20 23:10:07 +0300

    Create Muon.sh

---
+++ Muon.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+logo_displayed=false
+
+function logo() {
+    if ! $logo_displayed; then
+        bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
+        logo_displayed=true
+    fi
+}
+
+function printGreen {
+    echo -e "\e[1m\e[32m${1}\e[0m"
+}
+
+function check {
+    while true; do
+        logo
+        printGreen "Виберіть, що ви хочете переглянути в ноді Muon:"
+        echo "1) Журнал логів"
+        echo "2) Інформація про ноду"
+        echo "3) Версію встановленої ноди"
+        echo "4) Назву вашої ноди"
+        echo "5) Вийти з меню"
+        read choice
+
+        if [[ $choice == "1" ]]; then
+            echo ""
+            printGreen "Статус ноди (online/version)"
+            echo ""
+            docker logs muon-node
+            echo ""
+        elif [[ $choice == "2" ]]; then
+            echo ""
+            printGreen "Інформація про вашу ноду(Гаманець,Peer ID,Node ID...)"
+            echo ""
+            curl http://localhost:8011/status | jq '.'
+            echo ""
+        elif [[ $choice == "3" ]]; then
+            echo ""
+            printGreen "Бекап ноди в /root/BACKUPNODES/Muon backup/"
+            echo ""
+            docker exec -it muon-node ./node_modules/.bin/ts-node ./src/cmd/index.ts keys backup > backup.json
+            mkdir -p /root/BACKUPNODES/Muon\ backup
+            mv /root/backup.json /root/BACKUPNODES/Muon\ backup/
+
+        elif [[ $choice == "4" ]]; then
+            echo ""
+            printGreen "Рестарт ноди Muon"
+            docker restart muon-node
+            echo ""
+        elif [[ $choice == "5" ]]; then
+            break 
+        else
+            echo "Невірний вибір."
+        fi
+    done
+}
+
+check
