1535a0712162479bc4c4ac37f960e54602cdf4ff
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-09-21 23:31:00 +0300

    Update and rename Logss55555C.sh to Logss555555C.sh

--- Logss555555C.sh
+++ Logss555555C.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+function printDelimiter {
+  echo "==========================================="
+}
+
+function printGreen {
+  echo -e "\e[1m\e[32m${1}\e[0m"
+}
+
 services=("lavad" "subspaced" "nibid")
 pids=()
 
 for service in "${services[@]}"; do
     echo ""
+    printDelimiter
     printGreen "Журнал логів ноди: $service"
+    printDelimiter
     echo ""
     sudo journalctl -u "$service" -f -o cat &
     pids+=($!)
