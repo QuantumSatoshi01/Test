e6eb5672be4febc95b101d04be4df9a618339c48
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-09-21 23:24:32 +0300

    Update and rename Logss5555SEC.sh to Logss55555SEC.sh

--- Logss55555SEC.sh
+++ Logss55555SEC.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+function printDelimiter {
+  echo "==========================================="
+}
+
+function printGreen {
+  echo -e "\e[1m\e[32m${1}\e[0m"
+}
+
 services=("lavad" "subspaced" "nibid")
 pids=()
 
@@ -8,7 +16,9 @@ active_services=$(systemctl list-units --type=service --state=active | awk '{pri
 for service in "${services[@]}"; do
     if echo "$active_services" | grep -q "$service.service"; then
         echo ""
+        printDelimiter
         printGreen "Журнал логів ноди: $service"
+        printDelimiter
         echo ""
         sudo journalctl -u "$service" -f -o cat &
         pids+=($!)
