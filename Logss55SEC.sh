f39594cf6a1ee4ec26fd029c0c2270dcec9ed8cf
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-09-21 23:14:45 +0300

    Update and rename Logss5SEC.sh to Logss55SEC.sh

--- Logss55SEC.sh
+++ Logss55SEC.sh
@@ -4,11 +4,17 @@ function printGreen {
     echo -e "\e[1m\e[32m${1}\e[0m"
 }
 
+function printDelimiter {
+  echo "==========================================="
+}
+
 services=("lavad" "subspaced" "nibid")
 
 for service in "${services[@]}"; do
     echo ""
+    printDelimiter
     printGreen "Журнал логів ноди: $service"
+    printDelimiter
     echo ""
     sudo journalctl -u "$service" -f -o cat &
     sleep 5
