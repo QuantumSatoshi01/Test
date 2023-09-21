6d19e67686f76b9ed6d0422c78d6e8dbe076e4da
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-09-21 23:11:15 +0300

    Update and rename Logs.5Sec to Logs5SEC.sh

--- Logs5SEC.sh
+++ Logs5SEC.sh
@@ -3,8 +3,13 @@
 services=("lavad" "subspaced" "nibid")
 
 for service in "${services[@]}"; do
-    echo "Логи для служби $service протягом 5 секунд:"
+    echo ""
+    printGreen "Журнал логів ноди: $service"
+    echo ""
     sudo journalctl -u "$service" -f -o cat &
     sleep 5
     sudo pkill -f "journalctl -u $service"
+    sleep 2
 done
+
+wait
