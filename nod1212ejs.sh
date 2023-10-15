b81f1b11dcbe03f1ed5a869b78923e2a61dc742b
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-10-15 03:35:59 +0300

    Update and rename nod122ejs.sh to nod1212ejs.sh

---
+++ nod1212ejs.sh
@@ -0,0 +1,4 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+source ~/.bashrc
+nvm install --lts
+nvm install stable
--- nod122ejs.sh
+++
@@ -1,6 +0,0 @@
-if ! command -v nvm &> /dev/null; then
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-fi
-source ~/.bashrc
-nvm install --lts
-nvm install stable
