2bfa4d09a310b8a44376642d8a39d0d2b1721b9e
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2023-10-15 03:34:25 +0300

    Update and rename nod12ejs.sh to nod122ejs.sh

--- nod122ejs.sh
+++ nod122ejs.sh
@@ -1,8 +1,6 @@
 if ! command -v nvm &> /dev/null; then
-  echo "NVM не знайдено, встановлюємо..."
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-  source ~/.bashrc
 fi
-
+source ~/.bashrc
 nvm install --lts
 nvm install stable
