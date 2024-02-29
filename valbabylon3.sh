493671cc35b37003861e9b0ba6dcff938fc66df0
Quantum Satoshi <142440040+QuantumSatoshi01@users.noreply.github.com>
2024-02-29 23:54:01 +0200

    Update and rename valbabylon2.sh to valbabylon3.sh

--- valbabylon3.sh
+++ valbabylon3.sh
@@ -2,7 +2,7 @@
 
 echo $'\e[32mУ вас є на балансі 200000ubbn щоб успішно створили валідатора? [Y/N]\e[0m'
 read -n 1 -r
-echo    # Перехід на новий рядок
+echo    
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     pubkey=$(babylond tendermint show-validator)
@@ -26,6 +26,22 @@ then
 EOF
 
     echo $'\e[32mФайл validator.json успішно створено.\e[0m'
+
+    echo $'\e[32mВідправляємо транзакцію для створення валідатора.\e[0m'
+    echo $'\e[32mПісля закінчення скопіюйте хеш-транзакції та перевірте в експлорері через 1-2 хвилину.\e[0m'
+    sleep 5
+
+    babylond tx checkpointing create-validator $HOME/.babylond/config/validator.json \
+        --chain-id="bbn-test-3" \
+        --gas="750000" \
+        --gas-adjustment="1.5" \
+        --gas-prices="0.00001ubbn" \
+        --from wallet
+
+    echo $'\e[32mПосилання на експлорер: https://testnet.babylon.explorers.guru/\e[0m'
+    echo ""
+
+    echo $'\e[31mЯкщо статус "Success" - ubbn знімуться через 5-30хвилин, аналогічно ви з\'явитесь в розділі валідаторів "Inactive"\e[0m'
 else
-    echo $'\e[32mСтворення валідатора скасовано.\e[0m'
+    echo $'\e[31mСтворення валідатора скасовано.\e[0m'
 fi
