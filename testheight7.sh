8ca2636a1f0361d6257e301727a4e839c90716c1
Quantum Satoshi <142440040+QuantumSatoshi01@users.noreply.github.com>
2024-03-19 00:32:15 +0200

    Update and rename testheight6.sh to testheight7.sh

--- testheight7.sh
+++ testheight7.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-# Переменные
+
+mkdir -p "$HOME/AutoBot/LavaBot/"
+
+
 chatid="6793256074"
 bottoken="6974867623:AAFLu4EljfS_FHCd8B1pxESQM_bL8FHUjTY"
 node="lavad"
 server_ip=$(hostname -I | awk '{print $1}')
 
-# Получение имени сервера из файла server_name.txt
+
 server_name=$(grep "^$server_ip=" "$HOME/AutoBot/server_name.txt" | cut -d '=' -f 2)
 
-# Получение значения Block Height
+
 block_height=$($node status 2>&1 | jq -r '.SyncInfo.latest_block_height // .sync_info.latest_block_height')
 
-# Получение информации о валидаторе
+
 validator_info=$($node q staking validator $(lavad keys show wallet --bech val -a) | grep -E "jailed")
 
-# Дополнительная команда для извлечения количества токенов
+
 tokens_info=$($node q staking validator $(lavad keys show wallet --bech val -a) | grep "tokens")
 
-# Форматирование информации о токенах
+
 tokens=$(echo "$tokens_info" | awk -F': ' '{print $2}' | tr -d '"')
 
-# Формирование сообщения
+
 message="Name: $server_name\nBlock Height: $block_height\nValidator Info:\n$validator_info\nTokens: $tokens"
 
-# Отправка сообщения в телеграм
+
 curl -s -X POST \
     -H 'Content-Type: application/json' \
     -d "{\"chat_id\": \"$chatid\", \"text\": \"$message\"}" \
