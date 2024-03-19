1d3881f75f17c40958580f4d33fb643ebb741c11
Quantum Satoshi <142440040+QuantumSatoshi01@users.noreply.github.com>
2024-03-19 18:58:39 +0200

    Update LavaBot.sh

--- LavaBot.sh
+++ LavaBot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#Створення директорії,якщо не існує
+
 mkdir -p "$HOME/AutoBot/LavaBot/"
 
-#Перемінні для тг, ноди
+
 chatid="6793256074"
 bottoken="6974867623:AAFLu4EljfS_FHCd8B1pxESQM_bL8FHUjTY"
 node="lavad"
@@ -13,19 +13,41 @@ server_ip=$(hostname -I | awk '{print $1}')
 server_name=$(grep "^$server_ip=" "$HOME/AutoBot/server_name.txt" | cut -d '=' -f 2)
 
 
-block_height=$($node status 2>&1 | jq -r '.SyncInfo.latest_block_height // .sync_info.latest_block_height')
+get_block_info() {
+    block_height=$($node status 2>&1 | jq -r '.SyncInfo.latest_block_height // .sync_info.latest_block_height')
+    if [ -n "$block_height" ]; then
+        echo "Block: $block_height"
+    else
+        echo "Block: N/A"
+    fi
+}
 
 
-validator_info=$($node q staking validator $(lavad keys show wallet --bech val -a) | grep -E "jailed")
+get_jailed_info() {
+    jailed_info=$($node q staking validator $(lavad keys show wallet --bech val -a) | grep -E "jailed")
+    if [ -n "$jailed_info" ]; then
+        echo "Validator: $jailed_info"
+    else
+        echo "Jailed: N/A"
+    fi
+}
 
 
-tokens_info=$($node q staking validator $(lavad keys show wallet --bech val -a) | grep "tokens")
+get_tokens_info() {
+    tokens_info=$($node q staking validator $(lavad keys show wallet --bech val -a) | grep -E "tokens")
+    if [ -n "$tokens_info" ]; then
+        tokens=$(echo "$tokens_info" | awk -F': ' '{print $2}' | tr -d '"')
+        echo "Tokens: $tokens"
+    else
+        echo "Tokens: N/A"
+    fi
+}
 
 
-tokens=$(echo "$tokens_info" | awk -F': ' '{print $2}' | tr -d '"')
-
-
-message="Name: $server_name\nBlock:   $block_height\n$validator_info\nTokens: $tokens"
+message="Name: $server_name\n"
+message+="$(get_block_info)\n"
+message+="$(get_jailed_info)\n"
+message+="$(get_tokens_info)"
 
 
 curl -s -X POST \
